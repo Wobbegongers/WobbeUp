fc94f0c38f92d17db5584dac439382d743374222
Danny <chungdanny63@yahoo.com>
2020-10-30 20:21:09 -0700

    backend set up

---
+++ controllers/productController.js
@@ -0,0 +1,8 @@
+const db = require('../models/databaseModel')
+const productController = {}
+
+
+
+
+
+module.exports = productController
---
+++ routes/accountRouter.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router()
+
+const accountController = require('../controllers/accountController')
+
+
+module.exports  = router
