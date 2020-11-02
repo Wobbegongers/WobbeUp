6f4a532daf67458830ede347ba57773c7d8ce784
Andy <a4heng@ucsd.edu>
2020-11-02 15:58:14 -0800

    redirect from profile to login

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -2,30 +2,36 @@ import React, { useState, useEffect } from 'react';
 import CardList from './CardList'
 import axios from 'axios';
 import { connect } from 'react-redux'
+import {Redirect} from 'react-router-dom'
 
 const url = 'http://localhost:3000/'
 
 const mapStateToProps = (state) => ({
-  user_id: state.wobbeReducer.user_id
+  user_id: state.wobbeReducer.user_id,
+  username: state.wobbeReducer.username
 })
 
 
 const Profile = (props) => {
 
   const [userListing, setUserListing] = useState([]);
+  const [redirect, setRedirect] = useState(false);
+
 
   useEffect(() => {
-    console.log("Profile Props:", props)
+    // console.log(props)
+    if(props.username ==='') setRedirect(true)
+    else{
     axios.get(url + 'listing/userItems', {
       params: {
         user_id: props.user_id
       }
     })
       .then(res => {
-        console.log(res.data)
         setUserListing(res.data)
       })
       .catch(err => console.log(err))
+    }
   }, []);
 
   const cardList = userListing.map((el, index) => {
@@ -33,9 +39,20 @@ const Profile = (props) => {
   });
 
   return (
+    redirect === false 
+    ?
     <div>
       {cardList}
     </div>
+    :
+    <Redirect 
+          to={{
+            pathname: '/login',
+            state:{
+                username: props.username
+            }
+        }}
+        />
   )
 };
 
